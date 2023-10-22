f15f5702c8f7e7723b3f9d65819fe7051651a33f
saidheeraj.y <saidheeraj.y15@iiits.in>
2023-10-21 21:12:25 -0400

    Configured mysql for the app

--- ss_server/ss_server/settings.py
+++ ss_server/ss_server/settings.py
@@ -75,8 +75,18 @@ WSGI_APPLICATION = 'ss_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SellSmart',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # Change this to the MySQL server's hostname or IP address if it's not on localhost.
+        'PORT': '3306',  # Change this if your MySQL server uses a different port.
+        'OPTIONS': {
+            'charset': 'utf8mb4',  # Use utf8mb4 for full Unicode support.
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'use_pure': True,  # Use the pure Python MySQL client (required for Django 4.0+).
+        },
+        'TIME_ZONE': 'UTC'
     }
 }
 
