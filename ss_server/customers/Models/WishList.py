2343a34cb352b0c080110ad515cb73706ad89999
saidheeraj.y <saidheeraj.y15@iiits.in>
2023-10-21 23:09:33 -0400

    item relation added

--- ss_server/customers/Models/WishList.py
+++ ss_server/customers/Models/WishList.py
@@ -1,9 +1,10 @@
 from django.db import models
 from .Customer import Customer
 from stores.models import Store
+from inventory.models import Item
 
 class WishList(models.Model):
     wishlist_id = models.AutoField(primary_key=True)
     customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
-    # item_id
+    item_id = models.ForeignKey(Item, on_delete=models.CASCADE)
     store_id = models.ForeignKey(Store, on_delete=models.CASCADE)
