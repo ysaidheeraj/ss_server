aec0b85f19117dc1340833eeb6d6b5ed3b56b962
saidheeraj.y <saidheeraj.y15@iiits.in>
2023-10-21 20:50:44 -0400

    Created customer model

---
+++ ss_server/customers/Models/Customer.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.core.validators import RegexValidator
+
+phone_regex = RegexValidator(
+    regex=r'^\+?1?\d{9,15}$'
+)
+
+class Customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    middle_name = models.CharField(max_length=100)
+    email_id = models.EmailField()
+    phone_number = models.CharField(
+        max_length=15,
+        validators=[phone_regex]
+    )
+    store_id = models.IntegerField()
+    profile_picture = models.BinaryField()
+
