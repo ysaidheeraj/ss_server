7339f2bb7a779511aad4519b66c4a2d2be536f37
saidheeraj.y <saidheeraj.y15@iiits.in>
2023-11-23 08:19:44 -0500

    App init and added routes

---
+++ ss_server/ss_client/src/App.js
@@ -0,0 +1,32 @@
+import { Header } from "./Components/Header";
+import { Footer } from "./Components/Footer";
+import { Container } from "react-bootstrap";
+import { HomePage } from "./pages/HomePage";
+import { ProductPage } from "./pages/ProductPage";
+import { CartPage } from "./pages/CartPage";
+import { LoginPage } from "./pages/LoginPage";
+import { RegisterPage } from "./pages/RegisterPage";
+import { ProfilePage } from "./pages/ProfilePage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+function App() {
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Routes>
+            <Route path="/" Component={HomePage} exact />
+            <Route path="/product/:id" Component={ProductPage} />
+            <Route path="/cart/:id?" Component={CartPage} />
+            <Route path="/login" Component={LoginPage} />
+            <Route path="/register" Component={RegisterPage} />
+            <Route path="/profile" Component={ProfilePage} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
