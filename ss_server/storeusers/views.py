4aa919d6101c30204d2f0cb4db6ed40574e4c5e1
saidheeraj.y <saidheeraj.y15@iiits.in>
2023-11-24 19:28:39 -0500

    image upload related changes

--- ss_server/storeusers/views.py
+++ ss_server/storeusers/views.py
@@ -163,7 +163,7 @@ class CustomerUpdateView(APIView):
             profile_picture.name = 'customer_'+str(user_info['store_id'])+"_"+str(customer.user_id)+'.'+ext
             customer.profile_picture = profile_picture
         
-        if data['password']:
+        if data.get('password'):
             data['password'] = make_password(data['password'])
         
         customer_record = StoreUserSerializer(customer, data=request.data, partial=True)
