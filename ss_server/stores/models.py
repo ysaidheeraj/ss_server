d12d1ef14b7b3fa00ae43231c041c3c476fe94c4
saidheeraj.y <saidheeraj.y15@iiits.in>
2023-11-28 23:23:19 -0500

    added name attrs to models for adminconsole

--- ss_server/stores/models.py
+++ ss_server/stores/models.py
@@ -5,3 +5,6 @@ class Store(models.Model):
     store_id = models.AutoField(primary_key=True)
     store_name = models.CharField(max_length=100)
     store_description = models.TextField(max_length=1000)
+
+    def __str__(self) -> str:
+        return self.store_name
